3a5a0f189be382d3a8b81611e4ae745f30f4851a
Camille Espanol <espanol@fullsix.com>
2015-10-09 22:22:52 +0200

    Adding monitor properties

--- common/src/main/java/moneyfactory/common/properties/EnumAvailableProperties.java
+++ common/src/main/java/moneyfactory/common/properties/EnumAvailableProperties.java
@@ -9,13 +9,19 @@ package moneyfactory.common.properties;
  */
 public enum EnumAvailableProperties {
 
+    // Plateform properties
     PLATEFORM_THING_ROOTNAME("plateformThingRootName", String.class, "moneyfactory"),
+
+    // Bot properties
     BOT_CONSUMER_KEY("botConsumerKey", String.class, null),
     BOT_CONSUMER_SECRET("botConsumerSecret", String.class, null),
     BOT_ACCESS_TOKEN("botAccessToken", String.class, null),
     BOT_ACCESS_TOKEN_SECRET("botAccessTokenSecret", String.class, null),
     BOT_UNIQUE_ID("botUniqueId", String.class, null),
-    BOT_PUBLISHED_TWEETS_DB("publishedDb", String.class, "publishedTweets.dat");
+    BOT_PUBLISHED_TWEETS_DB("publishedDb", String.class, "publishedTweets.dat"),
+
+    // Monitoring properties
+    MONITORING_TYPE("monitoringType", String.class, "dweet");
 
     private String propertyName;
     private Class type;
